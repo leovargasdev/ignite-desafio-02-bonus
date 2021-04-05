3a80ec2d348ded1379f0f153281806214e1e16e1
Leonardo Vargas <leu1607@hotmail.com>
2021-04-05 20:59:27 -0300

    Migrando o header para ts

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 import Food from '../../components/Food';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
 import ModalAddFood from '../../components/ModalAddFood';
 import ModalEditFood from '../../components/ModalEditFood';
 
